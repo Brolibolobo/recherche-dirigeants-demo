0be7764466cd38fd9a8023393a3265448ce534ac
ASM <hermes-bot@example.com>
2026-07-27 13:40:50 +0000

    chore: connect production Supabase backend

--- src/central-config.js
+++ src/central-config.js
@@ -1,4 +1,4 @@
 export const centralConfig = Object.freeze({
-  url: '',
-  publicKey: '',
+  url: "https://qvyxzgsqnmcmnphbxtqx.supabase.co",
+  publicKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2eXh6Z3Nxbm1jbW5waGJ4dHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNTQxMTEsImV4cCI6MjEwMDczMDExMX0.CVFGpZPaKDXDzJqEMuxZq9wygBljfVQYFHS59hZQP5s",
 });
